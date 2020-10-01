8fe4a322c0cd451c26a31f20355c4d377e24c5e7
sanketkothiya <63290489+sanketkothiya@users.noreply.github.com>
2020-10-01 21:00:05 +0530

    use with function

--- Quiz-1.py
+++ Quiz-1.py
@@ -1,8 +1,12 @@
 #create A Calculator For Addition Of two Number
 
-print("Enter Your First Number")
-n1=input()
-print("Enter Your Second Number")
-n2=input()
 
-print("Sum of This Two Numbers is", int(n1)+int(n2))
+
+n1=int(input("Enter the number"))
+
+n2=int(input("Enter the number"))
+
+def sum(n1,n2):
+  n3=n1+n2;
+  print(n3);
+
