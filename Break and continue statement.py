8a7f56398e08fb2ac541ddaffa8f68599b455b14
Akshay Koladiya <60186474+akshay2437@users.noreply.github.com>
2020-10-16 18:12:33 +0530

    update breakpoint

--- Break and continue statement.py
+++ Break and continue statement.py
@@ -1,10 +1,10 @@
 i=0
-
+#in this code you can apply for loop
 while(True):
     if i+1<8:
         i = i + 1
         continue                    #loop run at this position
-
+#break loop is use to out of the loop
     print(i+1, end=" ")
     if (i==44):
         break                       #stop the loop
