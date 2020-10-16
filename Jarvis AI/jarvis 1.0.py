5695ce4e84124d980b260f4312469657ed918c16
Akshay Koladiya <60186474+akshay2437@users.noreply.github.com>
2020-10-16 09:53:58 +0530

    it require to execute.

--- Jarvis AI/jarvis 1.0.py
+++ Jarvis AI/jarvis 1.0.py
@@ -6,6 +6,12 @@ import webbrowser
 import os
 import smtplib
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
