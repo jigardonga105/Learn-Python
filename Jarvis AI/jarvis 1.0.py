6e65dd982c07af147b20532ce37b3350743229cd
Jigar Donga <69411775+jigardonga105@users.noreply.github.com>
2020-10-16 09:48:58 +0530

    Update jarvis 1.0.py

--- Jarvis AI/jarvis 1.0.py
+++ Jarvis AI/jarvis 1.0.py
@@ -6,13 +6,6 @@ import webbrowser
 import os
 import smtplib
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-# print(voices[0].id)
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
